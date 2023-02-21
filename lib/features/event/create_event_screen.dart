84d79367794dc5719f6c0e484b7d75aca5d52bf2
Gracjan <gracjan122@protonmail.com>
2023-02-21 21:40:31 +0100

    Create simple stepper

--- lib/features/event/create_event_screen.dart
+++ lib/features/event/create_event_screen.dart
@@ -1,4 +1,6 @@
+// import "package:event_app/api/models/event.dart";
 import "package:flutter/material.dart";
+// import "package:intl/intl.dart";
 
 class CreateEventScreen extends StatefulWidget {
   const CreateEventScreen({super.key});
@@ -8,13 +10,102 @@ class CreateEventScreen extends StatefulWidget {
 }
 
 class _State extends State<CreateEventScreen> {
+  final _formKey = GlobalKey<FormState>();
+  int currentStep = 0;
+  int id = 0, authorId = 0;
+  String title = "", description = "";
+  DateTime startsAt = DateTime.now();
+  DateTime? endsAt;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Event creation"),
+        title: const Text("Event creation"),
       ),
-      body: Text("dupa"),
+      body: Container(
+        child: Stepper(
+          type: StepperType.horizontal,
+          steps: getSteps(),
+          currentStep: currentStep,
+          onStepCancel: () => currentStep == 0
+              ? null
+              : setState(() {
+                  currentStep -= 1;
+                }),
+          onStepContinue: () {
+            bool isLastStep = currentStep == getSteps().length - 1;
+            if (isLastStep) {
+              //TODO: send event to backend
+              return;
+            }
+            setState(() {
+              currentStep += 1;
+            });
+          },
+        ),
+      ),
     );
+  }
+
+  List<Step> getSteps() {
+    return <Step>[
+      Step(
+        state: currentStep > 0 ? StepState.complete : StepState.indexed,
+        title: const Text("Description"),
+        content: Column(
+          children: [
+            TextFormField(
+              maxLength: 50,
+              textInputAction: TextInputAction.next,
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Please enter a title";
+                }
+                return null;
+              },
+              onChanged: (value) {
+                title = value;
+              },
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Sleepover at Mickey's",
+                labelText: "Enter event title",
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              maxLines: 5,
+              maxLength: 1000,
+              textInputAction: TextInputAction.next,
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Please enter a description";
+                }
+                return null;
+              },
+              onChanged: (value) {
+                description = value;
+              },
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Third edition of coding event app together!",
+                labelText: "Enter event description",
+              ),
+            )
+          ],
+        ),
+      ),
+      Step(
+        state: currentStep > 1 ? StepState.complete : StepState.indexed,
+        title: const Text("Time & Place"),
+        content: Column(
+          children: [
+            // Location picker
+            // Date picker
+          ],
+        ),
+      )
+    ];
   }
 }
