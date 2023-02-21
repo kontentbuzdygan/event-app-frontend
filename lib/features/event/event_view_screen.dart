96d4bc992df7060e9f0c025b551148c29313b70e
geraldserafin <serafingerald@protonmail.com>
2023-02-22 00:42:11 +0100

    Fetch author profile on event view

--- lib/features/event/event_view_screen.dart
+++ lib/features/event/event_view_screen.dart
@@ -1,21 +1,34 @@
 import "package:event_app/api/models/event.dart";
+import "package:event_app/api/models/profile.dart";
 import "package:event_app/features/shared/loading_screen.dart";
 import "package:event_app/features/shared/not_found_screen.dart";
 import "package:flutter/material.dart";
 import "package:intl/intl.dart";
 
-class EventViewScreen extends StatelessWidget {
-  EventViewScreen({super.key, required int id}) {
-    event = Event.find(id);
-  }
 
-  // TODO: Chain event fetching with author profile fetching
-  late final Future<Event> event;
+class EventData {
+  Event event;
+  Profile author;
+
+  EventData({required this.event, required this.author});
+}
+
+class EventViewScreen extends StatelessWidget {
+  const EventViewScreen({super.key, required this.id});
+
+  final int id;
+
+  Future<EventData> fetchEventData() async {
+    final event = await Event.find(id);
+    final author = await Profile.find(event.authorId);
+
+    return EventData(event: event, author: author);
+  }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: event,
+      future: fetchEventData(),
       builder: (context, snapshot) {
         if (snapshot.hasError) {
           return const NotFoundScreen(
@@ -29,30 +42,33 @@ class EventViewScreen extends StatelessWidget {
 
         final DateFormat formatter = DateFormat("yyyy-MM-dd");
 
+        final event = snapshot.data!.event;
+        final author = snapshot.data!.author;
+
         return Scaffold(
           appBar: AppBar(
-            title: Text(snapshot.data!.title),
+            title: Text(event.title),
           ),
           body: Padding(
             padding: const EdgeInsets.all(10.0),
             child: Center(
               child: Column(
                 children: [
-                  infoRow("Author ID: ", snapshot.data!.authorId.toString()),
+                  infoRow("Author display name: ", author.displayName),
                   infoRow(
                     "starts at ",
-                    formatter.format(snapshot.data!.startsAt),
+                    formatter.format(event.startsAt),
                     TextStyle(
                       color: Colors.blue[700],
                       fontWeight: FontWeight.normal,
                     ),
                   ),
-                  if (snapshot.data!.endsAt != null)
+                  if (event.endsAt != null)
                     infoRow(
                       "Ends At: ",
-                      formatter.format(snapshot.data!.endsAt!),
+                      formatter.format(event.endsAt!),
                     ),
-                  infoRow("Description: ", snapshot.data!.description),
+                  infoRow("Description: ", event.description),
                 ],
               ),
             ),
