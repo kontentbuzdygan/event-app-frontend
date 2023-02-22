50b3d6296172321e0bd07b0ea944d99aef3a86f3
Michał Wawrzynowicz <michalwa2003@gmail.com>
2023-02-22 10:38:54 +0100

    Add tests for AuthState and RestClient improvements (#17)
    * Make AuthState testable
    
    * Simplify RestClient and add joinUrl utility
    
    * Add hasBeenCalled matcher
    
    * Add tests for remaining methods
    
    * Remove trailing commas lint
    
    * Tweaks
    
    * Manual formatting
    
    * Move some files around
    
    * Rename MockSecureStorage to InMemoryStorage
    
    * Extend AuthState tests
    
    * Revert manual formatting in some files
    
    * Rename joinUrl to joinPath and use it in RestClient

--- lib/api/rest_client.dart
+++ lib/api/rest_client.dart
@@ -7,55 +7,43 @@ import "package:event_app/main.dart";
 import "package:event_app/utils.dart";
 import "package:http/http.dart" as http;
 
+RestClient _rest = RestClient();
+RestClient get rest => _rest;
+
+void overrideRestClient(RestClient value) {
+  _rest = value;
+}
+
 class RestClient {
-  static Future<JsonObject> post(List<dynamic> path, [JsonObject body = const {}]) async {
-    final baseUrl = dotenv.get("API_URL");
-    final res = await http.post(
-      Uri.parse("$baseUrl/${path.join("/")}"),
-      headers: _headers(),
-      body: jsonEncode(body),
-    );
+  final _http = http.Client();
 
+  Future<JsonObject> get(List<dynamic> path) => makeRequest("GET", path);
+  Future<JsonObject> post(List<dynamic> path, [JsonObject body = const {}]) =>
+      makeRequest("POST", path, body);
+  Future<JsonObject> delete(List<dynamic> path, [JsonObject body = const {}]) =>
+      makeRequest("DELETE", path, body);
+
+  Future<JsonObject> makeRequest(
+    String method,
+    List<dynamic> path, [
+    JsonObject? body,
+  ]) async {
+    final baseUrl = dotenv.get("API_URL");
+    final uri = Uri.parse("$baseUrl/${joinPath(path)}");
+    final request = http.Request(method, uri);
+    request.headers.addAll(_headers);
+    request.body = jsonEncode(body);
+
+    final res = await _http.send(request);
     try {
-      return res.json();
+      return await res.json();
     } on Unauthorized {
       App.authState.deleteUserToken();
       rethrow;
     }
   }
 
-  static Future<JsonObject> get(List<dynamic> path) async {
-    final baseUrl = dotenv.get("API_URL");
-    final res = await http.get(
-      Uri.parse("$baseUrl/${path.join("/")}"),
-      headers: _headers(),
-    );
-
-    try {
-      return res.json();
-    } on Unauthorized {
-      App.authState.deleteUserToken();
-      rethrow;
-    }
-  }
-
-  static Future<JsonObject> delete(List<dynamic> path, [JsonObject body = const {}]) async {
-    final baseUrl = dotenv.get("API_URL");
-    final res = await http.delete(
-      Uri.parse("$baseUrl/${path.join("/")}"),
-      headers: _headers(),
-      body: jsonEncode(body),
-    );
-
-    try {
-      return res.json();
-    } on Unauthorized {
-      App.authState.deleteUserToken();
-      rethrow;
-    }
-  }
-
-  static Map<String, String> _headers() => {
+  Map<String, String> get _headers => {
         HttpHeaders.contentTypeHeader: "application/json; charset=UTF-8",
         HttpHeaders.acceptHeader: "application/json",
         if (App.authState.loggedIn)
--- lib/utils.dart
+++ lib/utils.dart
@@ -3,7 +3,17 @@ import "dart:convert";
 import "package:event_app/api/exceptions.dart";
 import "package:http/http.dart";
 
-extension StatusClasses on Response {
+/// Converts the given elements to strings and joins them with slashes, ensuring
+/// there is no consecutive or leading/trailing slashes
+String joinPath(List<dynamic> parts) =>
+  parts
+    .expand((part) => part
+      .toString()
+      .split("/")
+      .where((innerPart) => innerPart.isNotEmpty))
+    .join("/");
+
+extension StatusClasses on BaseResponse {
   /// True for 2xx status codes
   bool get isSuccess => 200 <= statusCode && statusCode < 300;
 }
@@ -13,6 +23,15 @@ extension JsonDecodeBody on Response {
   /// throws [InvalidResponseStatus]
   dynamic json() {
     if (isSuccess) return jsonDecode(body);
+    throw InvalidResponseStatus.of(statusCode);
+  }
+}
+
+extension JsonDecodeBodyStreamed on StreamedResponse {
+  /// Calls [jsonDecode] on the request body if the status code is `2xx`, otherwise
+  /// throws [InvalidResponseStatus]
+  Future<dynamic> json() async {
+    if (isSuccess) return jsonDecode(await stream.bytesToString());
     throw InvalidResponseStatus.of(statusCode);
   }
 }
