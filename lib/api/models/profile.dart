e32ddbeff8598d3504509a6d71f46d5c12bb4ca5
Gerald <61117794+geraldserafin@users.noreply.github.com>
2023-02-28 19:45:10 +0100

    Add profile model (#22)
    * Abstract common pattern in RestClient methods and add patch method
    
    * Create profile model
    
    * Fetch author profile on event view
    
    * Implement fetchAuthor method for event model
    
    * Fix error handling
    
    ---------
    
    Co-authored-by: michalwa <michalwa2003@gmail.com>

---
+++ lib/api/models/profile.dart
@@ -0,0 +1,26 @@
+import "package:event_app/api/json.dart";
+import "package:event_app/api/rest_client.dart";
+
+const String _apiPath = "profiles";
+
+class Profile {
+  final int id;
+  final String displayName;
+  final String? bio;
+
+  const Profile._({
+    required this.id,
+    required this.displayName,
+    this.bio,
+  });
+
+  factory Profile.fromJson(JsonObject json) => Profile._(
+        id: json["id"],
+        displayName: json["display_name"],
+        bio: json["bio"],
+      );
+
+  static Future<Profile> find(int id) async {
+    return Profile.fromJson(await rest.get([_apiPath, id]));
+  }
+}
